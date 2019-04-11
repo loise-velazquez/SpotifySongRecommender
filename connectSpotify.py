935953135bd306c93774a6cd9f2848b1666103d2
nbigger <nick.bigger@gmail.com>
2019-04-10 18:38:07 -0700

    added logic for pulling 250 most recently saved songs and automatically populating songs file

--- connectSpotify.py
+++ connectSpotify.py
@@ -5,17 +5,27 @@ import pprint
 import csv
 import Song;
 
-username = "1136204305"
+def populateSongFile(sp):
+    offset = 0
+    with open('data/songs.csv', mode='w') as songs_file:
+        for x in range(5):
+            results = sp.current_user_saved_tracks(50, offset)
 
-scope = 'user-library-read'
-token = util.prompt_for_user_token(username, scope)
+            for item in results['items']:
+                track = item['track']
 
-if token:
-    sp = spotipy.Spotify(auth=token)
+                songs_writer = csv.writer(songs_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                trackID = track['id']
+
+                songs_writer.writerow([trackID])
+
+        offset += 50
+
+def printSongFile(sp):
     songIDs = []
-    with open('data/songs.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
+    with open('data/songs.csv') as songs_file:
+        songs_list = csv.reader(songs_file, delimiter=',')
+        for row in songs_list:
             songIDs.append(row[0])
     # results = sp.current_user_saved_tracks()
     # for item in results['items']:
@@ -26,5 +36,25 @@ if token:
     for id in songIDs :
       print sp.audio_features([id])
 
-else:
-    print "Can't get token for", username
+def main():
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        # print "Usage: %s username" % (sys.argv[0],)
+        # sys.exit()
+        username = "1136204305"
+
+    scope = 'user-library-read'
+    token = util.prompt_for_user_token(username, scope)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+
+        populateSongFile(sp)
+        printSongFile(sp)
+
+    else:
+        print "Can't get token for", username
+
+if __name__ == "__main__":
+    main()
