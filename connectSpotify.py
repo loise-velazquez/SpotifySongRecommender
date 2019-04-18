cfdbbe4c314084b2cd50d8e26ccb52b7773f6b6d
nbigger <nick.bigger@gmail.com>
2019-04-18 15:22:12 -0700

    implemeted nueral network using dummy data. real data preprocessing still an issue

--- connectSpotify.py
+++ connectSpotify.py
@@ -8,6 +8,9 @@ import pandas
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report,confusion_matrix
 from sklearn.metrics import accuracy_score
 
 def populate_export_csv(sp):
@@ -74,18 +77,40 @@ def get_audio_features(sp):
 def decisionTree(sp):
   populate_export_csv(sp)
 
-  data = pandas.read_csv('data/export.csv')
+  data = pandas.read_csv('data/data.csv', usecols = lambda column : column not in 
+["song_title" , "artist"])
   print data.describe()
+  print data.shape
 
-  train, test = train_test_split(data, test_size=0.15)
-  print ("Train size: ", len(train))
-  print ("Test size: ", len(test))
+  # train, test = train_test_split(data, test_size=0.15)
+  # print ("Train size: ", len(train))
+  # print ("Test size: ", len(test))
 
-  classifier = DecisionTreeClassifier(min_samples_split=100)
-  labels = ["duration_ms", "key", "mode", "time_signature", "acousticness", "danceability", "energy", "instrumentalness", "liveness", "loudness", "speechiness", "valence", "tempo"]
+  X = data.drop('target',axis=1)
+  y = data['target']
+
+  X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+  # classifier = DecisionTreeClassifier(min_samples_split=100)
+  # labels = ["duration_ms", "key", "mode", "time_signature", "acousticness", "danceability", "energy", "instrumentalness", "liveness", "loudness", "speechiness", "valence", "tempo"]
 
   # x_train = train[labels]
   # y_train = train["target"] # we need a field to define whether the user liked the song or not
+
+  scaler = StandardScaler()
+  scaler.fit(X_train)
+  StandardScaler(copy=True, with_mean=True, with_std=True)
+
+  X_train = scaler.transform(X_train)
+  X_test = scaler.transform(X_test)
+
+  mlp = MLPClassifier(hidden_layer_sizes=(13,13,13),max_iter=500)
+  mlp.fit(X_train,y_train)
+
+  predictions = mlp.predict(X_test)
+
+  print(confusion_matrix(y_test,predictions))
+  print(classification_report(y_test,predictions))
 
   # x_test = test[labels]
   # y_test = test["target"]
