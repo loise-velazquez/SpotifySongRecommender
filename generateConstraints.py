6a588d8c89051ddec09bcc1c63bee5e083052928
Aaron Thompson <athompson812@hotmail.com>
2019-05-09 17:05:25 -0700

    recommended song list is now being returned

--- generateConstraints.py
+++ generateConstraints.py
@@ -4,6 +4,7 @@ import spotipy.util as util
 import pprint
 import csv
 import warnings
+import random
 
 import pandas
 
@@ -16,30 +17,43 @@ import pandas
     #next do constraint satisfaction stuff
 
 def recommendedSongs(sp, classifier):
-        print("hello")
         data = pandas.read_csv(
             'data/data.csv', usecols=lambda column: column in ["song_title", "artist"])
-        print("hello2")
-        """
         rows = data.shape[0]
         recommendedSongs = []
-        for i in range(0, rows):
-                print(i)
+        
+        
+        for counter in range(0,50):
+                i = random.randint(0, rows-1)
                 songName = data.loc[i]['song_title']
                 artist = data.loc[i]['artist']
-                results = sp.search(q=[songName,artist], type="track", limit=1)
+                results = sp.search(q=[songName, artist], type="track", limit=1)
+
                 items = results['tracks']['items']
+                
                 if (len(items) is not 0):
                         track = items[0]
                         features = sp.audio_features(track['id'])
+                        
                         df = pandas.DataFrame(features)
+                        
                         df.to_csv('data/song.csv')
                         song = pandas.read_csv('data/song.csv', usecols=lambda column: column not in
                                         ["uri", "track_href", "analysis_url", "id", "type", "duration_ms"])
-                        if classifier.predict(song)[0] is 1:
-                                recommendedSongs.append(song)
-        
+                        
+                        #print("hello")
+                        prediction = classifier.predict(song)[0]
+                        print(type(prediction))
+                        if int(prediction) is 1:
+                                print("spaghetti")
+                                recommendedSongs.append(track)
+                        else:
+                                print("if Olob battled Thanos Olob would win")
+
+                        
+        print(len(recommendedSongs))
         for i in range(0,len(recommendedSongs)-1):
                 print(recommendedSongs[i])
         return recommendedSongs
-        """
+        
+        
